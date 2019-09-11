6e6438e5cc6cc9b3e51de5d11995f406f4e43e45
Matthew Parker <Matthew.Parker@SAS.com>
2019-09-10 22:55:58 -0400

    initial commit

---
+++ randomWord.py
@@ -0,0 +1,12 @@
+
+import random
+import csv
+
+
+with open('words.csv', newline='') as csvfile:
+    data = list(csv.reader(csvfile))
+
+print ('Here are 5 words')
+print(random.sample(data,k=5))
+  
+  
